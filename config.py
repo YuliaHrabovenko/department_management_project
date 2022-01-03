bc6bbf5df088601406f877d73b257ac49570dfb6
julse-lia <yuliia.hrabovenko@gmail.com>
2022-01-03 20:52:12 +0200

    config file added

---
+++ config.py
@@ -0,0 +1,26 @@
+"""
+File with database configuration.
+"""
+import os
+import secrets
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv()
+
+user = os.environ.get('MYSQL_USER')
+password = os.environ.get('MYSQL_PASSWORD')
+server = os.environ.get('MYSQL_SERVER')
+database = os.environ.get('MYSQL_DATABASE')
+
+
+# pylint: disable=too-few-public-methods
+class Config:
+    """
+    Config class.
+    """
+    DEBUG = True
+    SECRET_KEY = secrets.token_hex(32)
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{user}:{password}@{server}/{database}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
