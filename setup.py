af5975a76e829d5ceb56ee9876703ca6293956da
julse-lia <yuliia.hrabovenko@gmail.com>
2022-01-03 20:50:31 +0200

    setup file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='departments_employees',
+    description='App to manage departments and employees.',
+    author='Yuliia Hrabovenko',
+    author_email='yuliia.hrabovenko@gmail.com',
+    packages=find_packages()
+)
